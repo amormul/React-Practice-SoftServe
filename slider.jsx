9c900febf41a7bbd54a6c09034597cdcbef94b1e
r0mank0valchuk <kovalchuk.roman@chnu.edu.ua>
2025-04-28 20:54:44 +0300

    Slider

---
+++ slider.jsx
@@ -0,0 +1,7 @@
+<Slider
+    value={value}
+    onChange={handleChange}
+    valueLabelDisplay="auto"
+    min={0}
+    max={100}
+/>
--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import MovieSlider from './components/MovieSlider';
+import Header from './components/Header';
 
 function App() {
   return (
