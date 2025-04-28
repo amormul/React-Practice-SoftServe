ed9e42ee7c5d73af6b397ec8ad70a19724a15024
NicuJun <musykstar@gmail.com>
2025-04-28 11:07:19 +0300

    almost done

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,16 @@ import './App.css'
 import Button from './components/Button.tsx'
 import ColorCheckbox from './components/CheckBox.tsx'
 import TextFields from './components/TextInput.tsx'
-
+import TypeSearch from "./components/SearchBar.tsx"
 function App() {
 
   return (
     <>
       <h1>Button-Test</h1>
-     <Button></Button>
+      <Button></Button>
       <hr />
       <h1>SearchBar-test</h1>
+      <TypeSearch></TypeSearch>
       <hr />
       <h1>CheckBox-Test</h1>
       <ColorCheckbox></ColorCheckbox>
--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -1,25 +1,47 @@
-import * as React from 'react';
+// import * as React from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
-
-export default function  TextFields() {
+import { red } from '@mui/material/colors';
+export default function TextFields() {
   return (
     <Box
       component="form"
-      sx={{ 
-        '& .MuiTextField-root': { m: 1, width: '25ch' },
-      }}
+      sx={{ '& .MuiTextField-root': { m: 1, width: '25ch' } }}
       noValidate
       autoComplete="off"
     >
       <div>
+
         <TextField
           id="outlined-textarea"
-          label="Comment" //User Coment Component ( or User Info )
-          placeholder="Placeholder"
+          label="Comment"
+          placeholder="Write a comment"
+          maxRows={3}
+          multiline
+          sx={{
+            '& .MuiOutlinedInput-root': {
+              backgroundColor: '#fff',
+              borderRadius: '10px',
+              transition: '0.5s',
+              color: '#000',
+              '& fieldset': {
+                borderColor: red[100],
+                color: '#fff',
+              },
+              '&:hover fieldset': {
+                borderColor: red[500],
+                color: '#fff',
+              },
+              '&.Mui-focused fieldset': {
+                borderColor: red[700],
+                color: '#fff',
+              },
+            },
+          }}
         />
+
+
       </div>
-     
     </Box>
   );
 }
