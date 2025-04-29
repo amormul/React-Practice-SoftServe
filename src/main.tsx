4cbebb1b6e2d54f0ec1a948b230ee9f9b7b80df6
Ruslan Semydotskyi <ruslansemidockii@gmail.com>
2025-04-29 10:41:49 +0300

    Added authorization form

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-
+import AuthForm from './AuthForm.tsx'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <AuthForm />
   </StrictMode>,
 )
