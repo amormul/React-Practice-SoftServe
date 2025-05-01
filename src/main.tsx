cf4358be03a0c23d8a13ecef7befd885fc9a7246
Ruslan Semydotskyi <ruslansemidockii@gmail.com>
2025-05-01 10:10:34 +0300

    Moved AuthForm.tsx to /components

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import AuthForm from './AuthForm.tsx'
+import AuthForm from './components/AuthForm.tsx'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <AuthForm />
